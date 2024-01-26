e23d8bfed5afe7acf3c57dac2b5413de8f7528fb
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-26 22:40:26 +0800

    final

--- printing.py
+++ printing.py
@@ -1,4 +1,9 @@
 # Initialize previous number
+previous_number = 0
+
 # Use 'for' a loop 
-# Calculate the sum
+for current_number in range(0, 10):  
+    sum = current_number + previous_number  # Calculate the sum
+    print(f"Current Number {current_number} Previous number {previous_number} Sum: {sum}")
+    previous_number = current_number
 # Print 
